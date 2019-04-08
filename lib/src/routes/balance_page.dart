044f77447d8bea8d9cf2ed00f7e3e29b7071a5c9
Raveesh Agarwal <hi@raveeshagarwal.in>
2019-04-08 20:07:50 +0530

    does some stuff

--- lib/src/routes/balance_page.dart
+++ lib/src/routes/balance_page.dart
@@ -1,10 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class BalancePage extends StatelessWidget {
+class BalancePage extends StatefulWidget {
+  @override
+  _BalancePageState createState() => _BalancePageState();
+}
+
+class _BalancePageState extends State<BalancePage> {
+  FirebaseUser user;
+
+  @override
+  initState() {
+    super.initState();
+    FirebaseAuth.instance.currentUser().then((FirebaseUser newUser) {
+      user = newUser;
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("balance page"),
-    );
+    return user == null
+        ? CircularProgressIndicator()
+        : StreamBuilder(
+            stream:
+                Firestore.instance.document('users/${user.uid}').snapshots(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot != null && snapshot.hasData) {
+                return Center(
+//                  child: Text('${snapshot.data.balance}'),
+                  child: Text('${snapshot.data['balance']}'),
+                );
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          );
   }
 }
--- lib/src/routes/transfer_funds_page.dart
+++ lib/src/routes/transfer_funds_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 
-class TransferFundsPage extends StatelessWidget {
+class TransferFundsPage extends StatefulWidget {
+  @override
+  _TransferFundsPageState createState() => _TransferFundsPageState();
+}
+
+class _TransferFundsPageState extends State<TransferFundsPage> {
+  String token;
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("TF page"),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Visibility(visible: token != null, child: Text('token: $token')),
+        RaisedButton(
+          onPressed: () async {
+            try {
+              dynamic result = await CloudFunctions.instance.call(
+                functionName: 'generateToken',
+                parameters: {
+                  "amount": 210,
+                  'secret_pin': '112233'
+                },
+              );
+              print(result);
+              setState(() {
+                token = result['data'];
+              });
+            } catch (error) {
+              setState(() {
+                token = error.toString();
+              });
+              print(error.message);
+              return;
+            }
+          },
+          child: Text("Generate Token"),
+        ),
+      ],
     );
   }
 }
