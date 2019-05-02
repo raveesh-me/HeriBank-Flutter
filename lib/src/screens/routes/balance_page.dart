9a031cea5cd94c2a63851adf58206c896665d79e
Raveesh Agarwal <hi@raveeshagarwal.in>
2019-05-02 06:22:19 +0530

    app complete

--- lib/src/screens/routes/balance_page.dart
+++ lib/src/screens/routes/balance_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_banking_system/src/current_balance_block.dart';
@@ -23,31 +24,76 @@ class _BalancePageState extends State<BalancePage> {
   Widget build(BuildContext context) {
     return user == null
         ? CircularProgressIndicator()
-        : Column(
-            children: <Widget>[CurrentBalanceBlock()],
-          );
+        : StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance
+                .collection("users/${user.uid}/transactions")
+                .snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData || snapshot.hasError) {
+                return CircularProgressIndicator();
+              }
+
+              var documents = snapshot.data.documents;
+              return CustomScrollView(
+                slivers: <Widget>[
+                  SliverToBoxAdapter(
+                    child: CurrentBalanceBlock(),
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    sliver: SliverToBoxAdapter(
+                      child: Center(
+                        child: Text(
+                          'Your Recent Transactions:',
+                          style: ThemeData.light().textTheme.display4.copyWith(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w300,
+                              ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (_, int index) {
+                        return TransactionTile(
+                            documentSnapshot: documents[index]);
+                      },
+                      childCount: documents.length,
+                    ),
+                  )
+                ],
+              );
+            });
   }
 }
 
-class TransactionsList extends StatelessWidget {
+class TransactionTile extends StatelessWidget {
+  final DocumentSnapshot documentSnapshot;
+
+  const TransactionTile({Key key, this.documentSnapshot}) : super(key: key);
+  int get amount => documentSnapshot.data['amount'];
+  bool get isReceived => documentSnapshot.data["type"] == "received";
+  String get otherPerson =>
+      documentSnapshot.data[isReceived ? 'sender' : 'receiver']["token"]
+          ["name"];
+  String get tNo => documentSnapshot.documentID;
+
   @override
   Widget build(BuildContext context) {
     var transactionNumberStyle =
         ThemeData.light().textTheme.display4.copyWith(fontSize: 11);
-    return ListView(
-      children: <Widget>[
-        Center(
-          child: ListTile(
-            title: Text('AMT: 300'),
-            isThreeLine: true,
-            subtitle: Text('From: Raveesh Agarwal (rishu)'),
-            trailing: Text(
-              'txn no:\n456987458965412563',
-              style: transactionNumberStyle,
-            ),
-          ),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      child: ListTile(
+        title: Text('AMT: $amount'),
+        isThreeLine: true,
+        subtitle: Text('${isReceived ? "From" : "To"} : $otherPerson'),
+        trailing: Text(
+          'txn no:\n$tNo',
+          style: transactionNumberStyle,
         ),
-      ],
+      ),
     );
   }
 }
